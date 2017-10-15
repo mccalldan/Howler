437038fdfdfbb645c9c1f80863559e7d1c0f6983
mccalldan <drm8266@gmail.com>
2017-10-15 14:48:39 -0400

    add

---
+++ server.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var Article = require('./models/Article.js');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
+
+//mongoose.connect('mongodb://localhost/nyt-reactsearch');
+mongoose.connect('mongodb://fsdsdffdad:1qaz2wsx@ds119585.mlab.com:19585/heroku_2hmsmm6k');
+
+
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
+});
+
+app.get('/', function(req, res){
+  res.sendFile('./public/index.html');
+})
+
+app.get('/api/saved', function(req, res) {
+
+  Article.find({})
+    .exec(function(err, doc){
+
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.send(doc);
+      }
+    })
+});
+
+app.post('/api/saved', function(req, res){
+
+  var newArticle = new Article({
+    title: req.body.title,
+    date: req.body.date,
+    url: req.body.url
+  });
+
+  newArticle.save(function(err, doc){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      res.json(doc);
+    }
+  });
+
+});
+
+app.delete('/api/saved/:id', function(req, res){
+
+  Article.find({'_id': req.params.id}).remove()
+    .exec(function(err, doc) {
+      res.send(doc);
+  });
+
+});
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
