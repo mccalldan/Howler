0d49f1f86681920a77dbfd382938d91622fb094d
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-10-26 18:29:58 -0400

    redo

--- server.js
+++ server.js
@@ -1,84 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-var Article = require('./models/Article.js');
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Run Morgan for Logging
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
-app.use(express.static('./public'));
-
-//mongoose.connect('mongodb://localhost/nyt-reactsearch');
-mongoose.connect('mongodb://fsdsdffdad:1qaz2wsx@ds119585.mlab.com:19585/heroku_2hmsmm6k');
-
-
-var db = mongoose.connection;
-
-db.on('error', function (err) {
-  console.log('Mongoose Error: ', err);
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
-db.once('open', function () {
-  console.log('Mongoose connection successful.');
-});
-
-app.get('/', function(req, res){
-  res.sendFile('./public/index.html');
-})
-
-app.get('/api/saved', function(req, res) {
-
-  Article.find({})
-    .exec(function(err, doc){
-
-      if(err){
-        console.log(err);
-      }
-      else {
-        res.send(doc);
-      }
-    })
-});
-
-app.post('/api/saved', function(req, res){
-
-  var newArticle = new Article({
-    title: req.body.title,
-    date: req.body.date,
-    url: req.body.url
-  });
-
-  newArticle.save(function(err, doc){
-    if(err){
-      console.log(err);
-      res.send(err);
-    } else {
-      res.json(doc);
-    }
-  });
-
-});
-
-app.delete('/api/saved/:id', function(req, res){
-
-  Article.find({'_id': req.params.id}).remove()
-    .exec(function(err, doc) {
-      res.send(doc);
-  });
-
-});
-
-
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
